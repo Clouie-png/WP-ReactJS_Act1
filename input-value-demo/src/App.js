8e9a21a66c4f8e4d0eb8909affe72184ba9905c1
Clouie-png <Clouie_lazatin@rcc.edu.ph>
2024-09-14 12:02:25 +0800

    1

---
+++ input-value-demo/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import InputComponent from './InputComponent';
+import Plus from './Plus';
+
+function App(){
+  return(
+    <div className="App">
+      <InputComponent/>
+
+      <Plus/>
+    </div>
+  );
+}
+
+export default App;
